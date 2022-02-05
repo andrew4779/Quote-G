3fd1237199721406f691a0be29a1b6484b1cbdeb
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-02-05 11:46:50 +0300

    new changes

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteDetailsComponent implements OnInit {
   @Input() quote!: Quote;
-  @Output() isComplete = new EventEmitter<boolean>();
+  // @Output() isComplete = new EventEmitter<boolean>();
 
-  quoteDelete(complete: boolean) {
-      this.isComplete.emit(complete);
-  }
+  // quoteDelete(complete: boolean) {
+  //     this.isComplete.emit(complete);
+  // }
   
 
   constructor() { }
